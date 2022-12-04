39c7c8a269f7f55754e8e325601b8953475968d5
Li <li@gmail.com>
2022-12-05 01:59:23 +0300

    feat: finish task hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileBuffer = fs.readFileSync('./files/fileToCalculateHashFor.txt');
+    const hash = crypto.createHash('sha256');
+    const finalHex = hash.update(fileBuffer).digest('hex');
+    console.log(finalHex);
 };
 
 await calculateHash();
