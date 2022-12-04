ed406044f34ed65535efe6be68e67d87217561e4
Li <li@gmail.com>
2022-12-05 00:53:32 +0300

    feat: finish zip task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,11 @@
+import zlib from 'zlib';
+import fs from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+    let grip = zlib.createGzip();
+    let rid = fs.createReadStream('./files/fileToCompress.txt');
+    let writ = fs.createWriteStream('./files/archive.gz');
+    rid.pipe(grip).pipe(writ);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,11 @@
+import zlib from 'zlib';
+import fs from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+    let unzip = zlib.createGunzip();
+    let ri = fs.createReadStream('./files/archive.gz');
+    let wr = fs.createWriteStream('./files/fileToCompress.txt');
+    ri.pipe(unzip).pipe(wr);
 };
 
 await decompress();
