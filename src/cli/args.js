48076cdc55dc4e3661f4f92f28579c0253710f73
Li <li@gmail.com>
2022-12-05 22:46:12 +0300

    feat: finish task args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,6 @@
 const parseArgs = () => {
-    // Write your code here 
+    let r = process.argv.slice(-2).map(it => it.split("=")).map((it) => it[0].substring(2) + " is " + it[1]).join(', ');
+    console.log(r);
 };
 
 parseArgs();
